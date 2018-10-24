09aff59e063a9e219501c74c30cc5edf865fd39d
Matt <mwoolnerdeakos@gmail.com>
2018-10-24 18:42:37 -0400

    Reorganized files

---
+++ lib/utils.py
@@ -0,0 +1,63 @@
+from .directories import *
+
+def _decode_img(serialized_img, height=512, width=512):
+    image = tf.decode_raw(serialized_img, tf.float32)
+    image = tf.reshape(image, (512, 512))
+    
+    return image
+
+def _parse_function(example_proto):
+    """Parses TF records
+    
+    Arguments:
+        example_proto {tf.Example} -- [description]
+    
+    Returns:
+        list -- A list in the form of [Id, Image, Labels]
+    """
+    features = {
+        'id' : tf.VarLenFeature(tf.string),
+        'height' : tf.FixedLenFeature([], tf.int64),
+        'width' : tf.FixedLenFeature([], tf.int64),
+        'image_green' : tf.FixedLenFeature([], tf.string),
+        'image_red' : tf.FixedLenFeature([], tf.string),
+        'image_blue' : tf.FixedLenFeature([], tf.string),
+        'image_yellow' : tf.FixedLenFeature([], tf.string),
+        'labels' : tf.VarLenFeature(tf.int64)
+    }
+
+    
+    parsed = tf.parse_single_example(example_proto, features)
+    
+    # Reconstruct the Images
+    channels = ['green', 'red', 'blue', 'yellow']
+    h = features['height']
+    w = features['width']
+    
+    images = tf.stack([_decode_img(
+                parsed[f'image_{color}'], height=h, width=w) 
+            for color in channels], axis=2)
+    
+    labels = parsed['labels']
+    labels = tf.sparse_to_dense(labels.indices, labels.dense_shape, labels.values)
+    
+    return parsed['id'], images, labels
+
+def _load_dataset(tfrecord_path):
+    dataset = tf.data.TFRecordDataset(tfrecord_path)
+    dataset.map(_parse_function)
+    return dataset
+
+def load_train():
+    path = f'{KAGGLE_TRAIN}/tfrecords/train.tfrecords'
+    return _load_dataset(path)
+
+def load_validation():
+    path = f'{KAGGLE_TRAIN}/tfrecords/val.tfrecords'
+    return _load_dataset(path)
+
+def load_test():
+    path = f'{KAGGLE_TRAIN}/tfrecords/test.tfrecords'
+    return _load_dataset(path)
+
+
--- write_records_script.py
+++ write_records_script.py
@@ -1,17 +1,20 @@
+import sys
+
 import tensorflow as tf 
 import matplotlib.image as im
 import numpy as np
 import pandas as pd
 import os
 from tqdm import tqdm
-from directories import *
-
-
+from config import *
 
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 def _int64_feature(value):
+    if isinstance(value, list):
+        return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+    # Else..
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 def load_image(image_id, directory=KAGGLE_TRAIN, channels=['green', 'blue', 'red', 'yellow']):
@@ -41,12 +44,11 @@ def extract_id(filename):
     """
     return filename.split('_')[0]
 
-def create_record(out_filename, file_directory, data):
+def create_record(out_filename, data):
     """Creates a TFRecord dataset
     
     Arguments:
-        out_filename {[type]} -- [description]
-        file_directory {[type]} -- [description]
+        out_filename {str} -- The path to the output file
         id_info {[type]} -- [description]
     """
     ids, labels = zip(*data)
@@ -65,7 +67,7 @@ def create_record(out_filename, file_directory, data):
             'image_red' : _bytes_feature(images['red'].tostring()),
             'image_blue' : _bytes_feature(images['blue'].tostring()),
             'image_yellow' : _bytes_feature(images['yellow'].tostring()),
-            'labels' : _bytes_feature(bytes(label, 'utf-8'))
+            'labels' : _int64_feature(label)
         }
 
         example = tf.train.Example(features=tf.train.Features(feature=feature))
@@ -76,8 +78,10 @@ def create_record(out_filename, file_directory, data):
 
 
 def main():
+    os.chdir('..')
     # Collect All Unique Ids and Labels and
     data = pd.read_csv(f'{KAGGLE_TRAIN}/train.csv')
+    data.Target = data.Target.apply(lambda x: [int(a) for a in x.split(' ')])
     data = list(zip(data.Id, data.Target))
     # Randomize and divide into 70% train, 15% validation and 15% test
     np.random.shuffle(data)
@@ -88,13 +92,13 @@ def main():
     test_data = data[int(0.85*N):]
 
     print("Writing Train Records")
-    create_record(f'{KAGGLE_TRAIN}/tfrecords/train.tfrecords', KAGGLE_TRAIN, train_data)
+    create_record(f'{KAGGLE_TRAIN}/tfrecords/train.tfrecords', train_data)
 
     print("Writing Validation Records")
-    create_record(f'{KAGGLE_TRAIN}/tfrecords/val.tfrecords', KAGGLE_TRAIN, val_data)
+    create_record(f'{KAGGLE_TRAIN}/tfrecords/val.tfrecords', val_data)
 
     print("Writing Test Records")
-    create_record(f'{KAGGLE_TRAIN}/tfrecords/test.tfrecords', KAGGLE_TRAIN, test_data)
+    create_record(f'{KAGGLE_TRAIN}/tfrecords/test.tfrecords', test_data)
 
 if __name__ == '__main__':
     main()
